5fec283644f7573fc742753830b4668d3e59a3f2
KareSteinsvik <kaste2085@hvl.no>
2025-09-17 19:21:04 +0200

    Add files via upload

---
+++ KarakterskalaC2.java
@@ -0,0 +1,42 @@
+package no.hvl.dat100;
+
+import static java.lang.Integer.parseInt;
+import static javax.swing.JOptionPane.showInputDialog;
+
+public class KarakterskalaC2 {
+
+
+
+    public static void main(String[] args) {
+
+        // Deklarasjon av variabler
+
+        int karakterSkala[] = {0, 40, 50, 60, 80, 90};
+        char bokstavKarakter[] = {'F', 'E', 'D', 'C', 'B', 'A'};
+        char karakter = ' ';
+
+        // les inn poengsumer 10 ganger
+        for (int n = 1; n < 11; n++) {
+            System.out.println("\nKarakter nr: " + n);
+
+            String inntekt = showInputDialog("Oppgi poengsum for kandidat nr: " +n);
+            int poengsum = parseInt(inntekt);
+
+            if (poengsum >= 0 && poengsum <= 100) {
+                // finn karakter
+                for (int i = 0; i < 6; i++) {
+                    if (poengsum >= karakterSkala[i]) {
+                        karakter = bokstavKarakter[i];
+                    }
+                }
+                // Utsktift
+                System.out.println("Karakter: " + karakter + "  gis for poengsum = " + poengsum);
+            } else {
+                n--;
+                System.out.println(poengsum + " er en ugyldig poengsum");
+
+
+            }
+        }
+    }
+}
